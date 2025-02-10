a9f46b8b1f90abc2ad1021d16c94c32075af043d
Andrew Mattingly <andrew@mattingly.com>
2025-02-11 10:57:14 +1100

    initial version

---
+++ src/copyin.c
@@ -0,0 +1,43 @@
+/*
+ * COPYIN - Utility function to copy a string to a buffer address held in a pointer
+ */
+
+#include <string.h>
+
+#include "macho.h"
+
+#ifdef __64BIT__
+#pragma export(COPYIN)
+#else
+#pragma prolog(COPYIN, main)
+#pragma epilog(COPYIN, main)
+#endif
+
+long COPYIN(
+        MACHO_STRING    string,        /* (input)        string to be copied */
+        MACHO_INTEGER   pstringLength, /* (input)        string length */
+        MACHO_POINTER   pbuffer,       /* (input)        buffer to receive string */
+        MACHO_INTEGER   pticks)        /* (output)       elapsed clock ticks */
+{
+    unsigned long       stringLength, ticks;
+    unsigned long long  start, end;
+
+    stringLength = *pstringLength;
+
+    __asm(" STCKF %[start]                  Get start timestamp"
+          : [start] "=m"(start)
+          :
+          : );
+
+    memcpy(*pbuffer, string, stringLength);
+
+    __asm(" STCKF %[end]                    Get end timestamp"
+          : [end]   "=m"(end)
+          :
+          : );
+
+    ticks = (unsigned long)(end - start);
+    *pticks = ticks;
+
+    return 0;
+}
