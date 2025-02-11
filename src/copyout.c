4f724401f0b4c1a8485294af99c74ff8b27661d2
Andrew Mattingly <andrew@mattingly.com>
2025-02-11 18:50:36 +1100

    initial version

---
+++ src/copyout.c
@@ -0,0 +1,43 @@
+/*
+ * COPYOUT - Utility function to copy from a buffer address held in a pointer to a string
+ */
+
+#include <string.h>
+
+#include "macho.h"
+
+#ifdef __64BIT__
+#pragma export(COPYOUT)
+#else
+#pragma prolog(COPYOUT, main)
+#pragma epilog(COPYOUT, main)
+#endif
+
+long COPYOUT(
+        MACHO_POINTER   pbuffer,       /* (input)        buffer containing string to be copied */
+        MACHO_INTEGER   pbufferLength, /* (input)        string length */
+        MACHO_STRING    string,        /* (output)       string */
+        MACHO_INTEGER   pticks)        /* (output)       elapsed clock ticks */
+{
+    unsigned long       bufferLength, ticks;
+    unsigned long long  start, end;
+
+    bufferLength = *pbufferLength;
+
+    __asm(" STCKF %[start]                  Get start timestamp"
+          : [start] "=m"(start)
+          :
+          : );
+
+    memcpy(string, *pbuffer, bufferLength);
+
+    __asm(" STCKF %[end]                    Get end timestamp"
+          : [end]   "=m"(end)
+          :
+          : );
+
+    ticks = (unsigned long)(end - start);
+    *pticks = ticks;
+
+    return 0;
+}
