756bf45dbdef8069b11ac3150f9d0e53d71b262e
Andrew Mattingly <andrew@mattingly.com>
2025-02-11 20:10:39 +1100

    initial version

---
+++ src/stcke.c
@@ -0,0 +1,34 @@
+/*
+ * STCKE - STORE CLOCK EXTENDED
+ */
+
+#include "macho.h"
+
+#ifdef __64BIT__
+#pragma export(STCKE)
+#else
+#pragma prolog(STCKE, main)
+#pragma epilog(STCKE, main)
+#endif
+
+long STCKE(
+        MACHO_STRING    D2,            /* (output)       16-byte (128-bit) buffer */
+        MACHO_INTEGER   pticks)        /* (output)       elapsed clock ticks */
+{
+    unsigned long       ticks;
+    unsigned long long  start, end;
+
+    __asm(" STCKF %[start]        Get start timestamp            \n"
+          " STCKE %[clock]        Invoke STCKE instruction       \n"
+          " STCKF %[end]          Get end timestamp                "
+          : [start]      "=m"(start),
+            [end]        "=m"(end),
+            [clock]      "=m"(D2[0])
+          :
+          : );
+
+    ticks = (unsigned long)(end - start);
+    *pticks = ticks;
+
+    return 0;
+}
